d989736e552650a2e001053d9e3a31f0d739e949
harshitverma2103 <harshitverma2103@gmail.com>
2022-08-14 00:39:29 +0530

    add sum method in calculator

--- calculator.java
+++ calculator.java
@@ -1,5 +1,10 @@
 class Calculator {
     public static void main(String[] args) {
-        System.out.println('Tere bhai ka Calculator');
+        System.out.println("Tere bhai ka Calculator");
+        System.out.println(sum(6, 5))
+    }
+
+    public static int sum(int firstNumber, int secondNumber){
+        return firstNumber + secondNumber;
     }
 }
