d3c22fb4c59dce589eb286541aec4c79dd2e69f7
harshitverma2103 <harshitverma2103@gmail.com>
2022-08-14 00:52:56 +0530

    add multiply method in calculator

--- calculator.java
+++ calculator.java
@@ -3,6 +3,7 @@ class Calculator {
         System.out.println("Tere bhai ka Calculator");
         System.out.println(sum(6, 5));
         System.out.println(subtract(6,5));
+        System.out.println(multiply(6,5));
     }
 
     public static int sum(int firstNumber, int secondNumber) {
@@ -11,5 +12,9 @@ class Calculator {
 
     public static int subtract(int firstNumber, int secondNumber) {
         return firstNumber - secondNumber;
+    }
+
+    public static int multiply(int firstNumber, int secondNumber) {
+        return firstNumber * secondNumber;
     }
 }
