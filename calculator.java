2099e82ef0a4409e4a7215d24858c27db60b715b
harshitverma2103 <harshitverma2103@gmail.com>
2022-08-14 00:44:47 +0530

    add subtract method in calculator

--- calculator.java
+++ calculator.java
@@ -1,10 +1,15 @@
 class Calculator {
     public static void main(String[] args) {
         System.out.println("Tere bhai ka Calculator");
-        System.out.println(sum(6, 5))
+        System.out.println(sum(6, 5));
+        System.out.println(subtract(6,5));
     }
 
-    public static int sum(int firstNumber, int secondNumber){
+    public static int sum(int firstNumber, int secondNumber) {
         return firstNumber + secondNumber;
+    }
+
+    public static int subtract(int firstNumber, int secondNumber) {
+        return firstNumber - secondNumber;
     }
 }
