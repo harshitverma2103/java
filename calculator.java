89f42e83bba1b64e428bf6fc35b67b861517e674
harshitverma2103 <harshitverma2103@gmail.com>
2022-08-14 13:05:13 +0530

    add divide method in calculator

--- calculator.java
+++ calculator.java
@@ -2,8 +2,9 @@ class Calculator {
     public static void main(String[] args) {
         System.out.println("Tere bhai ka Calculator");
         System.out.println(sum(6, 5));
-        System.out.println(subtract(6,5));
-        System.out.println(multiply(6,5));
+        System.out.println(subtract(6, 5));
+        System.out.println(multiply(6, 5));
+        System.out.println(divide(6, 4));
     }
 
     public static int sum(int firstNumber, int secondNumber) {
@@ -16,5 +17,9 @@ class Calculator {
 
     public static int multiply(int firstNumber, int secondNumber) {
         return firstNumber * secondNumber;
+    }
+
+    public static float divide(float firstNumber, float secondNumber) {
+        return firstNumber / secondNumber;
     }
 }
