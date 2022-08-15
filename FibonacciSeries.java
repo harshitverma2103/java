0cdbd921c74171d3b5687bc1ee0cc41c96532507
harshitverma2103 <harshitverma2103@gmail.com>
2022-08-15 13:11:54 +0530

    add class FibonacciSeries

---
+++ FibonacciSeries.java
@@ -0,0 +1,32 @@
+public class FibonacciSeries {
+    public static void main(String[] args) {
+        getFibonacciSeries(8);
+        printSeries(getFibonacciSeries(8));
+    }
+
+    public static int[] getFibonacciSeries(int length) {
+        int[] fibonacciSeries = new int[length];
+        int leftNum = 0;
+        int rightNum = 1;
+        int result = 0;
+        if (length == 0) {
+            return fibonacciSeries;
+        }
+
+        fibonacciSeries[0] = leftNum;
+
+        for (int i = 1; i < length; i++) {
+            fibonacciSeries[i] = rightNum;
+            result = leftNum + rightNum;
+            leftNum = rightNum;
+            rightNum = result;
+        }
+        return fibonacciSeries;
+    }
+
+    public static void printSeries(int[] arr) {
+        for(int i =0; i < arr.length; i++){
+            System.out.println(arr[i]);
+        }
+    }
+}
