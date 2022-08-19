f685a1630e557f8f8f173b03535a98fd3d971577
harshitverma2103 <harshitverma2103@gmail.com>
2022-08-19 13:52:50 +0530

    add changeNumberToArray and printCharArray methods

--- ArrayProblemSolver.java
+++ ArrayProblemSolver.java
@@ -2,6 +2,8 @@ public class ArrayProblemSolver {
     public static void main(String[] args) {
         int[] numberArray = { 1, 8, 3, 4, 5 };
         System.out.println(checkAscendingOrder(numberArray));
+        char[] arr = changeNumberToArrray(20345);
+        printCharArray(arr);
     }
 
     public static boolean checkAscendingOrder(int[] numberArray) {
@@ -11,5 +13,29 @@ public class ArrayProblemSolver {
             }
         }
         return true;
+    }
+
+    public static char[] changeNumberToArrray(int inputNumber) {
+        int digitCount = 0;
+        String numberString = "";
+        {
+            while (inputNumber > 0) {
+                int lastDigit = inputNumber % 10;
+                numberString = numberString + lastDigit;
+                digitCount = digitCount + 1;
+                inputNumber = inputNumber / 10;
+            }
+            char[] characterArray = new char[digitCount];
+            for (int index = 0; index < characterArray.length; index++) {
+                characterArray[index] = numberString.charAt(4 - index);
+            }
+            return characterArray;
+        }
+    }
+
+    public static void printCharArray(char[] arr) {
+        for (int index = 0; index < arr.length; index++) {
+            System.out.println(arr[index]);
+        }
     }
 }
